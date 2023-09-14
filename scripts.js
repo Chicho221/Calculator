d24b3ab8f5ef67020400b8c6b8095c4b555e8765
Chicho221 <135489598+Chicho221@users.noreply.github.com>
2023-09-14 18:17:44 +0200

    Add function for getting values form display.
    Add reset button (Resets to default values and clears display).

--- scripts.js
+++ scripts.js
@@ -13,9 +13,11 @@ let operator = null;
 
 const numberBtns = document.querySelectorAll('[data-number]');
 const operatorBtns = document.querySelectorAll('[data-operator]');
-const clearBtn = document.querySelector('#clear');
+const resetBtn = document.querySelector('#reset');
+const equalsBtn = document.querySelector('#equalsBtn');
 const display = document.querySelector('.display');
 
+//Number Buttons
 numberBtns.forEach((button) =>{
     button.addEventListener('click',() => appendNumber(button.textContent))
 })
@@ -23,23 +25,43 @@ numberBtns.forEach((button) =>{
 function appendNumber(number){
     display.textContent += number;
 }
-
+//Operator Buttons
 operatorBtns.forEach((button) =>{
     button.addEventListener('click',() => setOperator(button.textContent))
 })
 
 function setOperator(operator){
-    display.textContent += operator;
+    display.textContent += ` ${operator} `;
 }
+//Get values from display
+function getValues(){
+    let valueString = display.textContent;
+    let valueArray = valueString.split(' ');
+    firstNumber = valueArray[0]
+    operator = valueArray[1]
+    nextNumber = valueArray[2]
+}
+//Show result
+equalsBtn.addEventListener('click',() => operate())
 
+//Clears display only (might need for more dynamic one)
 function clearDisplay(){
     display.textContent = '';
 }
+//Resets to default values and clears display
+resetBtn.addEventListener('click', () => resetCalculator())
+function resetCalculator(){
+    display.textContent = '';
+    firstNumber  = '';
+    nextNumber = '';
+    operator = null;
+}
 
-
+//Does calculations
 function operate(){
-    a = firstNumber;
-    b = nextNumber;
+    getValues();
+    a = parseInt(firstNumber);
+    b = parseInt(nextNumber);
     oper = operator;
 
     switch(oper){
@@ -57,7 +79,7 @@ function operate(){
 
 //Addition fuction
 function addition(a, b){
-    return a += b;
+    display.textContent = a + b;
 //    return console.log(sum)
 }
 //Subtraction function
